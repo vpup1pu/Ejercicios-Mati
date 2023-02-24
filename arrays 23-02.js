a9014c12fd110df3132a833510dcbe98f5373a10
Ainhi <ainhoabelenvargas@gmail.com>
2023-02-24 19:20:10 -0300

    terminado

--- arrays 23-02.js
+++ arrays 23-02.js
@@ -6,8 +6,19 @@
 //console.log(imprimirArray(['a','t',true])) retorna 'a' 't' true
 //console.log(imprimirArray([false,true,[1,2,3]])) retorna false true [1,2,3]
 
-function imprimirArray(){
+function imprimirArray(array){
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+}
+console.log(imprimirArray([1,2,3]))
 
+function imprimirHorizontal(array) {
+    let lista = array[0];
+    for (let i = 0; i < array.length; i++) {
+        lista = lista + ' ' + array[i];
+    }
+    return lista;
 }
 
 //Ejercicio 2:
@@ -16,8 +27,65 @@ function imprimirArray(){
 //a) Escribir un algoritmo con los pasos a seguir
 //b)Crear la funcion
 //Ejemplo:
-//console.log(imprimirArray([1,2,3])) // 2
-//console.log(imprimirArray([1,4,12,2,12,43])) //4 12 2 12
+//console.log(soloPares([1,2,3])) // 2
+//console.log(soloPares([1,4,12,2,12,43])) //4 12 2 12
+
+//a) Algoritmo Es_Par
+//DE: Numero (entero)
+//DS: par(booleano)
+//DA: -
+//Comienzo
+//par <- false
+//Si numero mod 2 = 0
+//  entonces 
+//      par <- true
+//fin Si_entonces
+//Escribir par
+//Fin Algoritmo
+
+//Algoritmo contar
+//DE: array (conjunto de enteros)
+//DS: cuenta
+//DA: elemento
+//Comienzo
+//cuenta <- 0
+//Para elemento desde 1 hasta N
+//  cuenta <- cuenta + 1
+//fin para
+//Escribir cuenta
+//Fin Algoritmo
+
+//Algoritmo Diferencia_Pares_e_Impares
+//DE: array (conjunto de enteros)
+//DS: array2 (conjunto de enteros)
+//DA: i, elemento
+//Comienzo
+//i <- 0
+//array2 <- 0
+//Mientras (i<contar(array)) hacer
+//  si Es_Par(elemento) 
+//      entonces
+//          array2 <- elemento
+//fin si_entonces
+//Escribir array2
+//Fin Algoritmo
+
+//b)
+function esPar(numero) {
+    var par = false;
+    if (numero % 2 === 0) {
+        par = true;
+    }
+    return par;
+}
+
+function soloPares(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (esPar(array[i])) {
+            console.log(array[i]);
+        }
+    }
+}
 
 //Ejercicio 3:
 //leer : https://www.delftstack.com/es/howto/javascript/javascript-append-elements/

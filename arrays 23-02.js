9b3d3b10bd9dded2fe4fa4acd987cb58f9f2dc83
Ainhi <ainhoabelenvargas@gmail.com>
2023-02-23 23:11:21 -0300

    checkpoint

--- arrays 23-02.js
+++ arrays 23-02.js
@@ -1,1 +1,23 @@
-//
+//Ejercicio 1:
+//crear una funcion que se llame imprimirArray, que recibe un array que contiene cualquier tamaño y cualquier
+//tipo de elementos e imprima en la consola todos sus elementos.
+//Ejemplos: *recordar que lo imprime verticalmente
+//console.log(imprimirArray([1,2,3])) retorna 1 2 3
+//console.log(imprimirArray(['a','t',true])) retorna 'a' 't' true
+//console.log(imprimirArray([false,true,[1,2,3]])) retorna false true [1,2,3]
+
+function imprimirArray(){
+
+}
+
+//Ejercicio 2:
+//Crear una funcion llamada soloPares que reciba un array de numeros enteros y que imprima solo los numeros 
+//pares
+//a) Escribir un algoritmo con los pasos a seguir
+//b)Crear la funcion
+//Ejemplo:
+//console.log(imprimirArray([1,2,3])) // 2
+//console.log(imprimirArray([1,4,12,2,12,43])) //4 12 2 12
+
+//Ejercicio 3:
+//leer : https://www.delftstack.com/es/howto/javascript/javascript-append-elements/
---
+++ bucles anidados.js
@@ -0,0 +1,29 @@
+//Ejercicio 1:
+//Escribir una funcion llamada dosFor -sin parametros- que contenga dos bucles for anidados,esto es 
+//un bucle for que contiene otro bucle adentro, los dos con indices de distinto nombre 
+//el primer for debera imprimir el siguiente mensaje: 'Primer For: indice vale : ' y debera mostrar el valor
+//del indice 
+//El segundo for debera imprimir el siguiente mensaje: 'Segundo For: indice vale : ' y debera mostrar el valor
+//del indice 
+//el tope del primer for (condicion de corte es cuando el indice es < 3) y el del segundo for tambien 
+//(condicion de corte es cuando indice es < 3) y de inciso agregale: cuantas veces aparece el mensaje 
+//'Primer For: ...' y cuantas veces aparece el mensaje 'Segundo For:...'
+
+function dosFor(){
+
+}
+
+//Ejercicio 2
+//a) Averiguar que significa la conversion de tipos.
+//b) leer https://www.w3schools.com/js/js_type_conversion.asp
+//c) Crear una funcion que imprima por pantalla los primero diez multiplos de la tabla del 1 al 5
+//nota: no necesariamente vas a utilizar lo visto arriba de forma explicita
+//console.log(tablaHastaCinco())
+//Salida:
+//1 2 3 4 5 6 7 8 9 10
+//2 4 6 8 10 12 14 16 18 20
+//3 6 9 12 15 18 21 24 27 30
+//4 8 12 16 20 24 28 32 36 40
+//5 10 15 20 25 30 35 40 45 50
+
+//nota: para lograr que se imprima en horizontal deberas crear un string y ir concatenando cada valor hasta llegar al ultimo multiplo
